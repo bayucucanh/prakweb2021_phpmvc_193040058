7a2aa799b459ca6263f9bec1a0eda3f4a636e15c
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2021-09-30 09:53:45 +0700

    Menyelesaikan latihan mengenai Assets di video ke 6

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,6 @@
+  <div class="container">
   <h1>About Me</h1>
+  <img src="<?= BASEURL;?>/img/bayu.JPG" alt="Bayu Cucan" width="200" class="rounded-circle shadow">
   <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya seorang <?= $data['pekerjaan']; ?></p>
+  </div>
+  
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,5 @@
-<h1>Selamat Datang di Website Saya</h1>
+<div class="container">
+  <h1>Selamat Datang di Website Saya</h1>
+</div>
 
  
