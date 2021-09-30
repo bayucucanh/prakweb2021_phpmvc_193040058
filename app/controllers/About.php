ef0d9fa85b8d692710866d2164ae5525cea58214
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2021-09-30 08:58:43 +0700

    Menyelesaikan latihan View di video ke 5

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,21 @@
 <?php 
 
-class About {
-  public function index($nama = 'nama', $pekerjaan = 'pekerjaan') {
-    echo "Halo, nama saya adalah $nama, saya adalah seorang $pekerjaan";
+class About extends Controller{
+  public function index($nama = 'nama kosong', $pekerjaan = 'pekerjaan kosong', $umur = 0) {
+    $data['nama'] = $nama;
+    $data['pekerjaan'] = $pekerjaan;
+    $data['umur'] = $umur;
+    $data['judul'] = 'About Me';
+
+    $this->view('templates/header', $data);
+    $this->view('about/index', $data);
+    $this->view('templates/footer');
   } 
   public function page()
   {
-    echo 'About/page';
+    $data['judul'] = 'Pages';
+    $this->view('templates/header', $data);
+    $this->view('about/page');
+    $this->view('templates/footer');
   } 
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,5 +1,7 @@
 <?php 
 // class Controller merupakan class utama yang mengatur semua controllers
 class Controller {
-  
+  public function view($view, $data = []) {
+    require_once '../app/views/' . $view . '.php';
+  }
 }
